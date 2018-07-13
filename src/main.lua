2b217565d5f118f673ad0c05ab87283fa09e3d72
Unknown <b6010572@my.shu.ac.uk>
2018-07-13 15:29:50 +0000

    Placed the player in the middle of the window

--- src/main.lua
+++ src/main.lua
@@ -3,9 +3,9 @@ function love.load()
 end
 
 function love.update(deltaTime)
-	
+	player:update(deltaTime)
 end
 
 function love.draw()
-	love.graphics.draw(player.image, 100, 100)
+	love.graphics.draw(player.image, player.x, player.y)
 end
--- src/player.lua
+++ src/player.lua
@@ -1,4 +1,11 @@
 player = {}
-player.image = love.graphics.newImage("../res/player.png")
+player.x = love.graphics.getWidth() / 2
+player.y = love.graphics.getHeight() / 2
+player.velocity = {}
+player.image = love.graphics.newImage("img/ghost.png")
+
+function player:update(deltaTime)
+	
+end
 
 return player
