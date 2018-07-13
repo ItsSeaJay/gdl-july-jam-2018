8f0cbcd6de5f22d7e1f2be84ef662bcd56ea803a
Unknown <b6010572@my.shu.ac.uk>
2018-07-13 15:35:38 +0000

    Made the player move

--- src/player.lua
+++ src/player.lua
@@ -2,10 +2,13 @@ player = {}
 player.x = love.graphics.getWidth() / 2
 player.y = love.graphics.getHeight() / 2
 player.velocity = {}
+player.velocity.x = 23
+player.velocity.y = 0
 player.image = love.graphics.newImage("img/ghost.png")
 
 function player:update(deltaTime)
-	
+	player.x = player.x + player.velocity.x * deltaTime
+	player.y = player.y + player.velocity.y * deltaTime
 end
 
 return player
