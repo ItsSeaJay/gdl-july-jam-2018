cf1a43b1d022fc28ccf5ebbef8768b3870fcee25
Unknown <b6010572@my.shu.ac.uk>
2018-07-14 13:55:15 +0000

    Tested that switching states works

--- src/player.lua
+++ src/player.lua
@@ -12,10 +12,40 @@ function Player:new(x, y)
 
 	-- Finite state machine
 	self.states = {}
-	self.states.test = function(self, deltaTime)
-		love.graphics.print("Test", 0, 0)
+	self.states.normal = function(self, deltaTime)
+		local up = love.keyboard.isDown("w") or love.keyboard.isDown("up")
+		local down = love.keyboard.isDown("s") or love.keyboard.isDown("down")
+		local left = love.keyboard.isDown("a") or love.keyboard.isDown("left")
+		local right = love.keyboard.isDown("d") or love.keyboard.isDown("right")
+
+		-- Horizontal
+		if up then
+			self.velocity.y = -self.speed
+		elseif down then
+			self.velocity.y = self.speed
+		else
+			self.velocity.y = 0
+		end
+
+		-- Vertical
+		if left then
+			self.velocity.x = -self.speed
+		elseif right then
+			self.velocity.x = self.speed
+		else
+			self.velocity.x = 0
+		end
+
+		-- Apply the velocity to the self's position
+		self.x = self.x + self.velocity.x * deltaTime
+		self.y = self.y + self.velocity.y * deltaTime
+
+		if love.keyboard.isDown("q") then self.state = self.states.test end
 	end
-	self.state = self.states.test
+	self.states.test = function(self, deltaTime)
+		if love.keyboard.isDown("e") then self.state = self.states.normal end
+	end
+	self.state = self.states.normal
 end
 
 -- Game loop
