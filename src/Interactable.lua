44e4cde0e19966f2390af9ed0e4e4663f117fb3a
Unknown <b6010572@my.shu.ac.uk>
2018-07-15 10:18:09 +0000

    Added interactable class

---
+++ src/Interactable.lua
@@ -0,0 +1,14 @@
+Object = require("classic")
+Interactable = Object:extend()
+
+function Interactable:new()
+
+end
+
+function Interactable:update(deltaTime)
+	
+end
+
+function Interactable:draw()
+	-- body
+end
